ac88332c8b3e9b9da658d21ff102d2f8a09e894e
Shahadat <shahadat2cp17@gmail.com>
2022-01-29 19:00:23 +0600

    1

---
+++ 1.js
@@ -0,0 +1,10 @@
+// Print in Console numbers from 1 to 10
+// Reverse 10 to 1
+
+for(var i = 1; i <= 10 ; i++){
+    console.log(i);
+}
+console.log()
+for( var j = 10; j >= 1; j--){
+    console.log(j)
+}
