6f81c415db2524183dd870e16b67056215e18474
Shahadat <shahadat2cp17@gmail.com>
2022-01-30 19:52:56 +0600

    p

---
+++ 01/01-inch-to-feet.js
@@ -0,0 +1,3 @@
+var feet = 15;
+var inches = feet * 12;
+console.log(inches + " Inches");
---
+++ 01/02-min-num-array.js
@@ -0,0 +1,3 @@
+var numbers = [55,43,67,48,32,77,5,51,66,34,65,87,98];
+const min = Math.min(...numbers);
+console.log("Smallest Number is ",min);
