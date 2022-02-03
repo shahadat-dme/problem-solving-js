1980d270422f03a8a5caf8456fae190930c29765
Shahadat <shahadat2cp17@gmail.com>
2022-02-03 23:03:07 +0600

    odd

---
+++ 02/04-sum-odd-num.js
@@ -0,0 +1,18 @@
+let Odd = 0;
+for (let i = 50; i <= 100; i += 2) {
+    Odd += i;
+}
+console.log(Odd);
+
+
+// let Odd = 0;
+// for (let i = 50; i <= 100; i ++) {
+//     Odd += i;
+// }
+// console.log(Odd);
+
+// let Odd = 0;
+// for (let i = 51; i <= 100; i += 2) {
+//     Odd += i;
+// }
+// console.log(Odd);
